a7049d6890336a6272249280c01fbe4c064ea61f
ErikKarlkvist <erikkarlkvist95@gmail.com>
2016-03-02 13:34:41 +0100

    loads of error

--- src/dat357/StudentPortal.java
+++ src/dat357/StudentPortal.java
@@ -15,8 +15,8 @@ import java.io.*;  // Reading user input.
 public class StudentPortal
 {
     /* TODO Here you should put your database name, username and password */
-    static final String USERNAME = "";
-    static final String PASSWORD = "";
+    static final String USERNAME = "tda357_052";
+    static final String PASSWORD = "erikemil";
 
     /* Print command usage.
      * /!\ you don't need to change this function! */
@@ -36,8 +36,8 @@ public class StudentPortal
             Class.forName("org.postgresql.Driver");
             String url = "jdbc:postgresql://ate.ita.chalmers.se/";
             Properties props = new Properties();
-            props.setProperty("tda357_052",USERNAME);
-            props.setProperty("erikemil",PASSWORD);
+            props.setProperty("user",USERNAME);
+            props.setProperty("password",PASSWORD);
             Connection conn = DriverManager.getConnection(url, props);
 
             String student = args[0]; // This is the identifier for the student.
